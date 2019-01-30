cc08590ea2a98aa60c620d00aeaf30e7ec662506
Mohammad Zubair Akrami <Zubair.akrami@afghan-wireless.com>
2019-01-30 12:23:29 +0430

    migration added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+Use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      Schema::defaultStringLength(191);
+
     }
 
     /**
