02d84af062cf102183f5f6090bb68ca12cc32936
Mohammad Zubair Akrami <Zubair.akrami@afghan-wireless.com>
2019-02-11 15:02:37 +0430

    active menue while clicked

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -79,7 +79,7 @@
           </li>
 
           <li class="nav-item has-treeview  ">
-            <a href="#" class="nav-link active">
+            <a href="#" class="nav-link  ">
               <i class="nav-icon fas fa-cogs"></i>
               <p>
                 Management
@@ -88,7 +88,7 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
+                <a href="#" class="nav-link  ">
                   <i class="fa fa-circle-o nav-icon"></i>
                   <p>Active Page</p>
                 </a>
