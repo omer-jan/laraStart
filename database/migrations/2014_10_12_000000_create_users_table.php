273a3c8ea3e07f50375117aa64f38de602c3a280
Mohammad Zubair Akrami <Zubair.akrami@afghan-wireless.com>
2019-02-11 15:40:42 +0430

    migration is creatd

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->mediumText('bio')->nullable();
+            $table->string('photo')->default('profile.png');
+            $table->string('type')->default("user");
+
             $table->rememberToken();
             $table->timestamps();
         });
