76562886f69eaca05c708a15c6c5c0ad481a8301
Mohammad Zubair Akrami <Zubair.akrami@afghan-wireless.com>
2019-02-12 10:51:39 +0430

    validation added

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -15,6 +15,11 @@ class UserController extends Controller
     }
     public function store(Request $request)
     {
+      $this->validate($request,[
+        'name' => 'required|string|max:191',
+        'email' => 'required|string|email|max:191|unique:users',
+        'password' => 'required|string|min:6',
+      ]);
         return User::create([
           'name' => $request['name'],
           'email' => $request['email'],
